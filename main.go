2533ef10f2bcd2f3ba66dbdf3132b12d0685d338
doorbash <milad.doorbash@gmail.com>
2021-12-09 09:03:28 +0330

    log fix

--- main.go
+++ main.go
@@ -30,8 +30,7 @@ func main() {
 	}
 
 	telegramBot.Handle(tb.OnText, func(m *tb.Message) {
-		log.Printf("message from %d (%s %s): %s", m.Sender.ID, m.Sender.FirstName, m.Sender.LastName, m.Text)
-		log.Println()
+		log.Printf("message from %d (%s %s): %s\n", m.Sender.ID, m.Sender.FirstName, m.Sender.LastName, m.Text)
 		if apiBot.text == "" || time.Since(apiBot.lastFetchTime).Minutes() >= 2 {
 			telegramBot.Send(m.Sender, "مشکلی رخ داد. لطفا بعدا دوباره امتحان کنید.")
 			return
